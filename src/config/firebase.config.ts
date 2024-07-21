2e0bf55f4fdc985cda120f552a404ae925469201
Callum Innes <me@cinnes.dev>
2024-07-21 17:18:47 +0100

    Update firebase.config.ts

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,6 +1,6 @@
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   // projectId: process.env.REACT_APP_FirebaseProjectId,
   // storageBucket: process.env.REACT_APP_FirebaseStorageBucket,
   // messagingSenderId: process.env.REACT_APP_FirebaseMessagingSenderId,
